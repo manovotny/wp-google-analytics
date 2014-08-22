d662fd3d4ac2c7bead3aeff9013ac9772988ad91
Michael Novotny <manovotny@gmail.com>
2014-08-22 10:10:33 -0500

    Removes defensive code in JavaScript.
    No need to check for tracking id if the server is already guarding against it.

--- js/bundle.concat.js
+++ js/bundle.concat.js
@@ -17,19 +17,15 @@
 
     var options = require('./google-analytics-options');
 
-    if ( options && options.trackingId && 0 < options.trackingId.length ) {
+    // Code Google Analytics gives you to add to your page.
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-        // Code Google Analytics gives you to add to your page.
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-        // Google Analytics settings.
-        ga( 'create', options.trackingId, 'auto' );
-        ga( 'send', 'pageview' );
-
-    }
+    // Google Analytics settings.
+    ga( 'create', options.trackingId, 'auto' );
+    ga( 'send', 'pageview' );
 
 }());
 },{"./google-analytics-options":1}]},{},[1,2]);
